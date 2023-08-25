44255f4e418b66d8dbf9c5aa60a4d1bf989585e1
jaemanc <jaemanc93@gmail.com>
2023-08-25 11:20:24 +0900

    # feature/chat-bot
    1. 페이지 엘리멘츠 못찾는 현상 테스트

--- macro/kakao_chatbot/chatbot_login.py
+++ macro/kakao_chatbot/chatbot_login.py
@@ -67,9 +67,16 @@ def login(request):
 
         logger.info(f' retried.. go! {login_web_site_url}, driver session id :  {login_page.session_id}')
     finally:
+
+        elements = login_page.find_elements(By.XPATH, '//img')
+
+        # 모든 엘리먼트 로그로 출력
+        for element in elements:
+            logging.info(f" Element: {element.tag_name} - Text: {element.text} , {element.get_attribute('outerHTML')} ")
+
         # 로그인 페이지 이동.
-        go_to_login_page_button = login_page.find_element(By.XPATH, '//img[@src="/images/gnb_login.gif"]')
-        go_to_login_page_button.click()
+        go_to_login_page_button = login_page.find_elements(By.XPATH, '//img[@src="/images/gnb_login.gif"]')
+        go_to_login_page_button[0].click()
 
         member_numbs = login_page.find_element(By.ID, "txtMember")
         member_numbs.send_keys(req_membershipNum)
